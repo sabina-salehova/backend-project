ff052663e43c1ea9623f1e04f5be6d9a33a261ed
sabina-salehova <sabinaas@code.edu.az>
2022-12-02 23:43:45 -0800

    front to back

---
+++ Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace back_project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace back_project.Areas.Admin.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
