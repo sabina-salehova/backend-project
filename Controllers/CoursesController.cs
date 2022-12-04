e745af8924d318098d5079cfe48cfb95b91308c2
sabina-salehova <sabinaas@code.edu.az>
2022-12-04 10:05:18 -0800

    added course details

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using back_project.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace back_project.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly AppDbContext _dbContext;
+
+        public CoursesController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Details(int id)
+
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if (id is null)
+                return NotFound();
+
+            var course = await _dbContext.Courses.Include(x=>x.Category).SingleOrDefaultAsync(t => t.Id == id);
+
+            if (course is null) return NotFound();
+
+            return View(course);
         }
     }
 }
