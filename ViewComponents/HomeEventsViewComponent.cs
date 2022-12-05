1b186ed8e1c54a6a860d65bc6289f05a73238fa0
[sabina-salehova] <[sabinaas@code.edu.az]>
2022-12-05 17:28:16 +0400

    added HomeEventViewComponent

---
+++ ViewComponents/HomeEventsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using back_project.DAL.Entities;
+using back_project.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_project.ViewComponents
+{
+    public class HomeEventsViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _dbContext;
+        public HomeEventsViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Event> events = await _dbContext.Events
+                .Where(s => !s.IsDeleted)
+                .ToListAsync();
+
+            return View(events);
+        }
+    }
+}
